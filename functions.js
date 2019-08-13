e5a1ec2d2efaf2799a8126c0893c3bfb905b67d8
ndm-it <38891361+ndm-it@users.noreply.github.com>
2019-08-13 12:46:53 -0700

    Adding and Testing anything I can

--- functions.js
+++ functions.js
@@ -6,10 +6,19 @@ window.addEventListener("load", function () {
   });
 });
 
-elli.script.subscribe('tpoApplication', 'loanOpen', onLoanOpened);
-async function onLoanOpened(proxy, loanData) {
-  console.log('before getting loan object');
-  const loanObj = elli.script.getObject('loan');
-  console.log('before logging loanObj');
-  console.log(loanObj);
+elli.script.subscribe('tpoApplication', 'loanOpen', displayEntireLoan);
+
+async function displayEntireLoan() {
+ // Retrieve the loan object by awaiting the result of the getObject() call
+ let loan = await elli.script.getObject("loan");
+
+ // Retrieve the entire encompass loan using another await
+ let completeLoanObjectFromHost = await loan.all();
+ console.log('The entire loan object looks like: ' + completeLoanObjectFromHost);
 }
+// async function onLoanOpened(proxy, loanData) {
+//   console.log('before getting loan object');
+//   const loanObj = elli.script.getObject('loan');
+//   console.log('before logging loanObj');
+//   console.log(loanObj);
+// }
