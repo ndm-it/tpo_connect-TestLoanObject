621967196e4af29f2471f301dd5cf7226bee6a2d
ndm-it <38891361+ndm-it@users.noreply.github.com>
2019-08-13 12:28:43 -0700

    updated functions

--- functions.js
+++ functions.js
@@ -7,21 +7,11 @@ window.addEventListener("load", function () {
   elli.script.getObject("tpoApplication").then(function (tpoAppObject) {
     this.tpoAppObject = tpoAppObject;
     GetUserData();
-    // This call will only succeed on page load event if user already has opened a loan in TPOC.
-    // Use the onLoanOpened event (as shown below) to get/refresh loan data when user opens a loan.
-      GetLoanData();
-      //myFunction();
   });
 });
 // Subscribe to loanOpen event to refresh context when loan context changes (context event handling).
 elli.script.subscribe('tpoApplication', 'loanOpen', onLoanOpened);
 function onLoanOpened(proxy, loanData) {
-  GetLoanData();
-  $("div#loanAvailable").show();
-  $("div#loanNotAvailable").hide();
-  $("div#processing").show();
-  this.loanData = loanData;
-  console.log('loanOpen => loanData ===', loanData);
 }
 // Sample methods for getting various data objects, just console logging for illustrative purposes.
 function GetUserData() {
@@ -37,55 +27,11 @@ function GetUserData() {
     console.log('User Profile Data ===', userProfileData);
     console.log('FirstName ===', userProfileData.FirstName);
     if (typeof userProfileData.FirstName === 'undefined') {
-      $('#userNotAvailable').show();
-      $('#userAvailable').hide();
-    } else {
-      $('#userNotAvailable').hide();
-      $('#userAvailable').show();
-        // Get a few fields for illustrative purposes.
-        $('#userName').html(userProfileData.FirstName + ' ' + userProfileData.LastName);
-      $('#userEmail').html(userProfileData.Email);
-      $('#userOrgId').html(userProfileData.ExternalOrgID);
-      $('#userId').html(userProfileData.ExternalUserID);
-    }
-  })
-    .catch(function (err) {
-      console.log('Fetch Error :-S', err);
-    });
-    }
 
-function GetLoanData() {
-  // Get loan summary and console log only.
-  tpoAppObject.getLoanSummary().then(function (loanSummary) {
-    console.log('Loan Summary ===', loanSummary);
-  })
-    .catch(function (err) {
-      console.log('Fetch Error :-S', err);
-    });
-  // Get full loan object, console log and populate some basic on screen information.
-  tpoAppObject.getLoanData().then(function (loanData) {
-      console.log('Loan Data ===', loanData);
-      appraisalCompany = loanData.UnderwriterSummary.OriginalAppraiser;
-    if (typeof loanData.LoanNumber === 'undefined') {
-      $('#loanNotAvailable').show();
-      $('#loanAvailable').hide();
     } else {
-      $('#loanNotAvailable').hide();
-      $('#loanAvailable').show();
-      // Get a few property address level fields for illustrative purposes.
-        $('#loanNumber').html(loanData.LoanNumber);
-        $('#loanGUID').html(loanData.EncompassId);
-        console.log(loanData.UnderwriterSummary);
-      $('#propertyStreet').html(loanData.Property.StreetAddress);
-      $('#propertyCity').html(loanData.Property.City);
-      $('#propertyState').html(loanData.Property.State);
-      $('#propertyZip').html(loanData.Property.PostalCode);
-      $('#appriaser').html(loanData.UnderwriterSummary.OriginalAppraiser);
-      $('#processing').hide();
-        $('#results').show();
     }
   })
     .catch(function (err) {
       console.log('Fetch Error :-S', err);
     });
-}
+    }
