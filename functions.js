6d19f0bc0c1d8c5d60efa846117e437c5e452585
ndm-it <38891361+ndm-it@users.noreply.github.com>
2019-08-13 12:43:34 -0700

    added log lines

--- functions.js
+++ functions.js
@@ -2,11 +2,14 @@ window.addEventListener("load", function () {
   elli.script.guest.create();
   elli.script.getObject("tpoApplication").then(function (tpoAppObject) {
     this.tpoAppObject = tpoAppObject;
+    console.log('referenced tpoAppObject');
   });
 });
 
 elli.script.subscribe('tpoApplication', 'loanOpen', onLoanOpened);
 async function onLoanOpened(proxy, loanData) {
+  console.log('before getting loan object');
   const loanObj = elli.script.getObject('loan');
+  console.log('before logging loanObj');
   console.log(loanObj);
 }
