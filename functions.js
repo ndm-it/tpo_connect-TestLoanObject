d6f8cec99d55dc67d548592d68641930a201e0cd
ndm-it <38891361+ndm-it@users.noreply.github.com>
2019-08-13 12:37:56 -0700

    added event listener and subscribe method

--- functions.js
+++ functions.js
@@ -1,2 +1,12 @@
-const loanObj = await elli.script.getObject('loan');
-console.log(loanObj);
+window.addEventListener("load", function () {
+  elli.script.guest.create();
+  elli.script.getObject("tpoApplication").then(function (tpoAppObject) {
+    this.tpoAppObject = tpoAppObject;
+  });
+});
+
+elli.script.subscribe('tpoApplication', 'loanOpen', onLoanOpened);
+async function onLoanOpened(proxy, loanData) {
+  const loanObj = elli.script.getObject('loan');
+  console.log(loanObj);
+}
