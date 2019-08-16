73d5c739f5887520c9632b167733b28226c0138a
ndm-it <38891361+ndm-it@users.noreply.github.com>
2019-08-16 07:07:28 -0700

    Updated function.js

--- functions.js
+++ functions.js
@@ -1,3 +1,5 @@
+var tpoAppObject = null;
+
 window.addEventListener("load", function () {
   elli.script.guest.create();
   elli.script.getObject("tpoApplication").then(function (tpoAppObject) {
@@ -8,13 +10,19 @@ window.addEventListener("load", function () {
 });
 
 elli.script.subscribe('tpoApplication', 'loanOpen', displayEntireLoan);
+function displayEntireLoan() {
 
-async function displayEntireLoan() {
+//async function displayEntireLoan() {
  // Retrieve the loan object by awaiting the result of the getObject() call
- let loan = await elli.script.getObject("loan");
-
+ //let loan = await elli.script.getObject("loan");
+ tpoAppObject.getUserData().then(function (userData) {
+   console.log('User Data ===', userData);
+ })
+   .catch(function (err) {
+     console.log('Fetch Error :-S', err);
+   });
  // Retrieve the entire encompass loan using another await
- let completeLoanObjectFromHost = await loan.all();
+ //let completeLoanObjectFromHost = await loan.all();
  console.log('The entire loan object looks like: ' + completeLoanObjectFromHost);
 }
 // async function onLoanOpened(proxy, loanData) {
