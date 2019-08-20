bc2c0c904dadb0d9432d6c2556c5982e0d20f39d
ndm-it <38891361+ndm-it@users.noreply.github.com>
2019-08-20 09:34:00 -0700

    REdid Function.js

--- functions.js
+++ functions.js
@@ -1,53 +1,102 @@
-var tpoAppObject = null;
+        var tpoAppObject = null;
+        var appraisalCompany = "";
+        var loanObj = {};
+    // Instantiate objects on window load (one-time event handling).
+    window.addEventListener("load", function () {
+      elli.script.guest.create();
+      elli.script.getObject("tpoApplication").then(function (tpoAppObject) {
+        this.tpoAppObject = tpoAppObject;
+        $("div#processing").show();
+        GetUserData();
+        $("div#processing").hide();
 
-window.addEventListener("load", function () {
-  elli.script.guest.create();
-  elli.script.getObject("tpoApplication").then(function (tpoAppObject) {
-    this.tpoAppObject = tpoAppObject;
-    console.log('referenced tpoAppObject');
-    console.log(tpoAppObject.getUserData());
-    //displayEntireLoan();
-  });
-});
+		    // This call will only succeed on page load event if user already has opened a loan in TPOC.
+		    // Use the onLoanOpened event (as shown below) to get/refresh loan data when user opens a loan.
+          GetLoanData();
+          GetAuthData();
+      });
+    });
+    // Subscribe to loanOpen event to refresh context when loan context changes (context event handling).
+     elli.script.subscribe('tpoApplication', 'loanOpen', onLoanOpened);
+     function onLoanOpened(proxy, loanData) {
+       $("div#processing").show();
+       GetLoanData();
+       $("div#loanAvailable").show();
+       $("div#loanNotAvailable").hide();
+       this.loanData = loanData;
+       console.log('loanOpen => loanData ===', loanData);
+     }
 
-elli.script.subscribe('tpoApplication', 'loanOpen', onLoanOpened);
-function onLoanOpened(proxy, loanData) {
-  $("div#processing").show();
-  GetLoanData();
-  $("div#loanAvailable").show();
-  $("div#loanNotAvailable").hide();
-  this.loanData = loanData;
-  console.log('loanOpen => loanData ===', loanData);
-}
 
-elli.script.subscribe('tpoApplication', 'loanOpen', displayEntireLoan);
-async function displayEntireLoan() {
+    // Sample methods for getting various data objects, just console logging for illustrative purposes.
+    function GetUserData() {
+      // Get basic user info and console log only.
+      tpoAppObject.getUserData().then(function (userData) {
+        console.log('User Data ===', userData);
+      })
+        .catch(function (err) {
+          console.log('Fetch Error :-S', err);
+        });
+      // Get full user profile, console log and populate some basic on screen information.
+      tpoAppObject.getUserProfileData().then(function (userProfileData) {
+        console.log('User Profile Data ===', userProfileData);
+        if (typeof userProfileData.FirstName === 'undefined') {
+          $('#userNotAvailable').show();
+          $('#userAvailable').hide();
+        } else {
+          $('#userNotAvailable').hide();
+          $('#userAvailable').show();
+            // Get a few fields for illustrative purposes.
+            $('#userName').html(userProfileData.FirstName + ' ' + userProfileData.LastName);
+          $('#userEmail').html(userProfileData.Email);
+          $('#userOrgId').html(userProfileData.ExternalOrgID);
+          $('#userId').html(userProfileData.ExternalUserID);
+        }
+      })
+        .catch(function (err) {
+          console.log('Fetch Error :-S', err);
+        });
+        }
 
-//async function displayEntireLoan() {
- // Retrieve the loan object by awaiting the result of the getObject() call
- //let loan = await elli.script.getObject("loan");
- tpoAppObject.getUserData().then(function (userData) {
-   console.log('User Data ===', userData);
- })
-   .catch(function (err) {
-     console.log('Fetch Error :-S', err);
-   });
+    function GetLoanData() {
+      // Get loan summary and console log only.
+      // tpoAppObject.getLoanSummary().then(function (loanSummary) {
+      //   console.log('Loan Summary ===', loanSummary);
+      // })
+      //   .catch(function (err) {
+      //     console.log('Fetch Error :-S', err);
+      //   });
+      // Get full loan object, console log and populate some basic on screen information.
+      tpoAppObject.getLoanData().then(function (loanData) {
+          console.log('Loan Data ===', loanData);
+          appraisalCompany = loanData.UnderwriterSummary.OriginalAppraiser;
+        if (typeof loanData.LoanNumber === 'undefined') {
+          $('#loanNotAvailable').show();
+          $('#loanAvailable').hide();
+        } else {
+          $('#loanNotAvailable').hide();
+          $('#loanAvailable').show();
+          // Get a few property address level fields for illustrative purposes.
+            $('#loanNumber').html(loanData.LoanNumber);
+            $('#loanGUID').html(loanData.EncompassId);
+            //console.log('UnderWriterSummary ===',loanData.UnderwriterSummary);
+          $('#propertyStreet').html(loanData.Property.StreetAddress);
+          $('#propertyCity').html(loanData.Property.City);
+          $('#propertyState').html(loanData.Property.State);
+          $('#propertyZip').html(loanData.Property.PostalCode);
+          $('#appriaser').html(loanData.UnderwriterSummary.OriginalAppraiser);
+          $('#processing').hide();
+            $('#results').show();
+            myFunction(appraisalCompany);
 
-   try {
-     let loanAuth = elli.script.getObject("auth");
-     //let loanObjectt = tpoAppObject.getObject("loan");
-     console.log(loanAuth);
-   }
-   catch(err) {
-     console.log("Could not retrieve auth object" + err);
-   }
- // Retrieve the entire encompass loan using another await
- //let completeLoanObjectFromHost = await loan.all();
- //console.log('The entire loan object looks like: ' + completeLoanObjectFromHost);
-}
-// async function onLoanOpened(proxy, loanData) {
-//   console.log('before getting loan object');
-//   const loanObj = elli.script.getObject('loan');
-//   console.log('before logging loanObj');
-//   console.log(loanObj);
-// }
+        }
+      })
+        .catch(function (err) {
+          console.log('Fetch Error :-S', err);
+        });
+    }
+
+    function GetAuthData() {
+      let authData = elli.script.getObject("auth");
+      console.log(authData);
+    }
