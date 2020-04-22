447d9e1f532f35bd1360f9c93635adacb9903148
ndm-it <38891361+ndm-it@users.noreply.github.com>
2020-04-21 18:06:33 -0700

    Create function2.js

---
+++ function2.js
@@ -0,0 +1,28 @@
+var tpoAppObject = null;
+var appraisalCompany = "";
+var loanObj = {};
+window.addEventListener("load", function () {
+elli.script.guest.create();
+elli.script.getObject("tpoApplication").then(function (tpoAppObject) {
+this.tpoAppObject = tpoAppObject;
+GetUserData();
+// This call will only succeed on page load event if user already has opened a loan in TPOC.
+// Use the onLoanOpened event (as shown below) to get/refresh loan data when user opens a loan.
+  GetLoanData();
+});
+});
+
+elli.script.subscribe('tpoApplication', 'loanOpen', onLoanOpened);
+function onLoanOpened(proxy, loanData) {
+$("div#processing").show();
+GetLoanData();
+$("div#loanAvailable").show();
+$("div#loanNotAvailable").hide();
+this.loanData = loanData;
+console.log('loanOpen => loanData ===', loanData);
+}
+
+function GetLoanData() {
+tpoAppObject.getLoanData().then(function (loanData) {
+console.log('Loan Data ===', loanData);
+}
