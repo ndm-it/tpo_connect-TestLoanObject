c51808e80bba2dafef2a99b4e14218effab82f49
ndm-it <38891361+ndm-it@users.noreply.github.com>
2020-04-21 18:17:39 -0700

    Update function2.js

--- function2.js
+++ function2.js
@@ -5,7 +5,7 @@ window.addEventListener("load", function () {
 elli.script.guest.create();
 elli.script.getObject("tpoApplication").then(function (tpoAppObject) {
 this.tpoAppObject = tpoAppObject;
-GetUserData();
+//GetUserData();
 // This call will only succeed on page load event if user already has opened a loan in TPOC.
 // Use the onLoanOpened event (as shown below) to get/refresh loan data when user opens a loan.
   GetLoanData();
